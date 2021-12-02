9feac02bf56b74f34a2eb3c80802b81494de4d9e
Jose Gajardo <joligato@gmail.com>
2021-12-02 12:00:55 -0300

    chore: organizando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import {HeaderComponent} from './shared/header/header.component';
+import {HeaderComponent} from './components/header/header.component';
 import {UsersComponent} from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
